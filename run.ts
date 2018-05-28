27c4bde9f21b4100ab0e35901158cb834ee9563c
belohlavek <dbhvk.96@gmail.com>
2018-05-28 12:29:05 -0300

    first commit

---
+++ run.ts
@@ -0,0 +1,25 @@
+import { tmpTest } from "./sandbox";
+import Commando, { Response } from "./Commando";
+
+tmpTest(async (dirPath, done) => {
+  new Commando(`dcl init`, {
+    silent: false,
+    workingDir: dirPath,
+    env: { DCL_ENV: "dev" }
+  })
+    .when(/Send anonymous usage stats to Decentraland?/, () => Response.YES)
+    .when(/Scene title/, () => "My test Scene\n")
+    .when(/Your ethereum address/, () => "0x\n")
+    .when(/Your name/, () => "John Titor\n")
+    .when(/Your email/, () => "john.titor@example.com\n")
+    .when(/Parcels comprising the scene/, () => "0,0\n")
+    .when(/Which type of project would you like to generate/, () => "static\n")
+    .endWhen(/Success! Run 'dcl preview' to see your scene/)
+    .on("err", e => console.log(e))
+    .on("end", async () => {
+      console.log("works");
+      done();
+    });
+}).then(() => {
+  process.exit(1);
+});
---
+++ sandbox.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as rimraf from "rimraf";
+
+export function tmpTest(fn: any) {
+  return new Promise(async (resolve, reject) => {
+    const name = "test-" + (+Date.now()).toString();
+    const dir = path.resolve(process.cwd(), name);
+    await fs.mkdir(dir);
+    const done = () => {
+      rimraf(dir, () => {
+        resolve();
+      });
+    };
+
+    try {
+      fn(dir, done);
+    } catch (e) {
+      rimraf(dir, () => {
+        reject(e);
+      });
+    }
+  });
+}
